ffb4d882fe12c83789d269a812166d167e2aaed1
Saswati Ghadai <83913522+Mittu0809@users.noreply.github.com>
2022-09-04 01:27:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(
+  <App />,
+  /*<div>
+    <Heading></Heading>
+    <p>This is a React js bootcamp taken up by Shaurya Sinha</p>
+  </div>,*/
+  document.getElementById("root")
+);
